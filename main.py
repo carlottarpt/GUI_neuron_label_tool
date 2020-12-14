b810427d0a0e7e6f6f43f04bd3ee4e8203d6d6e7
Carlotta Ruppert <1624314@stud.hs-mannheim.de>
2020-12-14 18:26:04 +0100

    update main

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from matplotlib.backends.backend_tkagg import (
  FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 from matplotlib.backend_bases import key_press_handler
+from matplotlib import cm
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,11 +22,11 @@ from load_data import *
 from PIL import Image, ImageTk
 from utils import *
 
-annotation_path_pca_ica = "/home/user/Documents/single_session/dataPackedForGeneration.mat"
+annotation_path_pca_ica = r"C:\Users\carlo\OneDrive - ZHAW\VT\Annotations\dataPackedForGeneration.mat"
 # annotation_path_cnmf_e = "/home/user/Documents/single_session/dataPackedForGeneration.mat"
 # annotation_path_min1pipe = "/home/user/Documents/single_session/dataPackedForGeneration.mat"
-cell_map_pca_ica = '/home/user/Documents/single_session/extracted/cellMap.mat'
-movie_path = '/home/user/Documents/single_session/preprocessed/preprocessedMovie.h5'
+cell_map_pca_ica = r'C:\Users\carlo\OneDrive - ZHAW\VT\Annotations\cellMap.mat'
+movie_path = r'U:\single_session\preprocessed\preprocessedMovie.h5'
 
 
 
@@ -38,9 +39,14 @@ class App(tk.Frame):
         self.window.geometry("550x300+300+150")
         self.window.resizable(width=True, height=True)
 
+
         self.frame_annotations_1 = tk.Frame(self.window, background="blue")
         self.frame_annotations_1.pack(side=tk.TOP, padx='5', pady='5')
         self.frame_annotations_1.configure(background="cyan")
+
+        self.canvas = tk.Canvas(self.frame_annotations_1, width=400, height=200)
+        self.canvas.pack()
+
         self.frame_buttons_1 = tk.Frame(master=self.window, background = "blue")
         self.frame_buttons_1.pack(side=tk.BOTTOM, padx='5', pady='5')
 
@@ -49,11 +55,14 @@ class App(tk.Frame):
 
         self.video = Video_Frames(movie_path)
         self.pca_ica = PCAICA(annotation_path_pca_ica)
+        self.neuron_pca_ica = None
+        self.cellmap_pca_ica = None
+
 
         #self.show_annotations()
         self.next_button = tk.Button(self.frame_buttons_1,
                              text="Next", fg="black",
-                             command=self.load_next_neuron())
+                             command=self.show_annotations)
         self.next_button.pack(side = tk.RIGHT)
         self.PCA_ICA_button = tk.Button(self.frame_buttons_2,
                                      text="PCA-ICA", fg="black",
@@ -65,32 +74,27 @@ class App(tk.Frame):
         self.CNMFE_button.pack(side = tk.LEFT)
         self.show_annotations()
 
-        #self.window.mainloop()
 
     def show_annotations(self):
-
+        self.canvas.delete("all")
         neuron = self.pca_ica.get_neuron()
         cell_map = self.pca_ica.get_cellmap()
+        cell_map /= np.max(cell_map)
 
         img = Image.fromarray(neuron)
-        img_cellmap = Image.fromarray(cell_map)
-
+        img_cellmap = Image.fromarray(np.uint8(cm.viridis(cell_map)*255))
+        #img_cellmap = img_cellmap.rotate
         basewidth = 100
         wpercent = (basewidth / float(img.size[0]))
         hsize = int((float(img.size[1]) * float(wpercent)))
         img = img.resize((basewidth, hsize), Image.ANTIALIAS)
         img_cellmap = img_cellmap.resize((basewidth, hsize), Image.ANTIALIAS)
-        img = ImageTk.PhotoImage(img)
-        img_cellmap = ImageTk.PhotoImage(img_cellmap)
-        canvas = tk.Canvas(self.frame_annotations_1, width=400, height=200)
-        canvas.pack()
-        canvas.create_image(0, 0, anchor="nw", image=img)
-        canvas.create_image(0, 100, anchor="nw", image=img_cellmap)
-        self.window.mainloop()
+        self.neuron_pca_ica = ImageTk.PhotoImage(img)
+        self.cellmap_pca_ica = ImageTk.PhotoImage(img_cellmap)
 
-    def load_next_neuron(self):
-        neuron = self.pca_ica.get_neuron()
-        return neuron
+        self.canvas.create_image(0, 0, anchor="nw", image=self.neuron_pca_ica)
+        self.canvas.create_image(0, 100, anchor="nw", image=self.cellmap_pca_ica)
+        #self.window.mainloop()
 
     def update(self):
         # Get a frame from the video source
@@ -111,6 +115,7 @@ class PCAICA():
         self.data = load_annotation_data(annotation_path_pca_ica, "pca_ica")
         self.masks = self.data[0]
         self.cellmap = load_cell_map(cell_map_pca_ica, "pca_ica")
+        self.cellmap = self.cellmap.transpose()
         self.counter = 0
         self.x_start = 0
         self.x_end = 0
@@ -126,7 +131,6 @@ class PCAICA():
 
     def get_cellmap(self):
         img = self.cellmap[self.x_start : self.x_end, self.y_start:self.y_end]
-        plt.imshow(img)
         return img
 
 
@@ -173,7 +177,7 @@ root.mainloop()
 
 #movie_frames = load_preprocessed_movie(movie_path)
 
-pca_ica = load_annotation_data(annotation_path_pca_ica, "pca_ica")
+#pca_ica = load_annotation_data(annotation_path_pca_ica, "pca_ica")
 #cnmf_e = load_annotation_data(annotation_path_cnmf_e, annotation_type)
 #min1pipe = load_annotation_data(annotation_path_min1pipe, annotation_type)
 
